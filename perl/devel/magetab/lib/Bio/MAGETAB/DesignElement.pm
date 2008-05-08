ed5938e8bd5e9ca3f78f17959f817b66fe6ae03c
Tim Rayner <tfrayner@gmail.com>
2008-05-08 17:03:41 +0000

    Now blocking direct instantiation of the abstract classes within the model.

--- perl/devel/magetab/lib/Bio/MAGETAB/DesignElement.pm
+++ perl/devel/magetab/lib/Bio/MAGETAB/DesignElement.pm
@@ -7,7 +7,17 @@ use Moose;
 
 BEGIN { extends 'Bio::MAGETAB::BaseClass' };
 
-# FIXME this is an abstract class; block direct instantiation.
+# This is an abstract class; block direct instantiation.
+sub BUILD {
+
+    my ( $self, $params ) = @_;
+
+    if ( blessed $self eq __PACKAGE__ ) {
+        confess("ERROR: Attempt to instantiate abstract class " . __PACKAGE__);
+    }
+
+    return;
+}
 
 no Moose;
 
