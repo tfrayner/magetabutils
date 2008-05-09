4dec8edb0d8715eaf73577ff8841b9c4385c1a4b
Tim Rayner <tfrayner@gmail.com>
2008-05-09 23:21:00 +0000

    Basic package structure with module load tests.

---
+++ perl/devel/magetab/lib/Bio/MAGETAB.pm
@@ -0,0 +1,15 @@
+# $Id$
+
+package Bio::MAGETAB;
+
+use Moose::Policy 'Moose::Policy::FollowPBP';
+use Moose;
+
+our $VERSION = 0.1;
+
+# Convenience class for module loading. May want to use a Module::List
+# type approach to just load everything in the Bio::MAGETAB namespace.
+
+no Moose;
+
+1;
