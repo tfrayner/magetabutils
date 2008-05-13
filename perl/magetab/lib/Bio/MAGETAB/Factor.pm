65b6f65eaf9f2b44bb375c389ec69676ddb3d858
Tim Rayner <tfrayner@gmail.com>
2008-05-13 12:47:25 +0000

    Added copyright notices and GPLv2 text.

--- perl/magetab/lib/Bio/MAGETAB/Factor.pm
+++ perl/magetab/lib/Bio/MAGETAB/Factor.pm
@@ -1,3 +1,20 @@
+# Copyright 2008 Tim Rayner
+# 
+# This file is part of Bio::MAGETAB.
+# 
+# Bio::MAGETAB is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+# 
+# Bio::MAGETAB is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with Bio::MAGETAB.  If not, see <http://www.gnu.org/licenses/>.
+#
 # $Id$
 
 package Bio::MAGETAB::Factor;
