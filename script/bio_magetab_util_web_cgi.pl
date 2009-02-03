89a456a6916cbf6480e7ff0b6ae1623d5e8da251
Tim Rayner <tfrayner@gmail.com>
2009-02-03 14:45:36 +0000

    Updated for initial 0.01 release.

--- script/bio_magetab_util_web_cgi.pl
+++ script/bio_magetab_util_web_cgi.pl
@@ -1,3 +1,24 @@
+#!/usr/bin/env perl
+#
+# Copyright 2008 Tim Rayner
+# 
+# This file is part of Bio::MAGETAB::Util::Web.
+# 
+# Bio::MAGETAB::Util::Web is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+# 
+# Bio::MAGETAB::Util::Web is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with Bio::MAGETAB::Util::Web.  If not, see <http://www.gnu.org/licenses/>.
+#
+# $Id$
+
 #!/usr/local/bin/perl -w
 
 BEGIN { $ENV{CATALYST_ENGINE} ||= 'CGI' }
