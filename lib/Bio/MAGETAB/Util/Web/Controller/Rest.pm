a821a270dca6748ad9727c46ce976871d811d0de
Tim Rayner <tfrayner@gmail.com>
2009-02-21 19:06:38 +0000

    Exploring one possible approach to a REST API.

--- lib/Bio/MAGETAB/Util/Web/Controller/Rest.pm
+++ lib/Bio/MAGETAB/Util/Web/Controller/Rest.pm
@@ -25,6 +25,8 @@ use strict;
 use warnings;
 use parent 'Catalyst::Controller::REST';
 
+use Scalar::Util qw( blessed );
+
 =head1 NAME
 
 Bio::MAGETAB::Util::Web::Controller::Rest - Catalyst Controller
@@ -37,6 +39,43 @@ Catalyst Controller supporting RESTful database access.
 
 =cut
 
+sub object_by_oid : Local : ActionClass('REST') {}
+
+sub object_by_oid_GET {
+
+    my ( $self, $c, $oid ) = @_;
+
+    my $object;
+    eval {
+
+        # Dies if not found.
+        $object = $c->model()->storage()->load( $oid );
+    };
+
+    if ( $object && ! $@ ) {
+        my $class = blessed $object;
+        my $method = $self->_summary_method( $class );
+        if ( UNIVERSAL::can( $self, $method ) ) {
+            $self->status_ok(
+                $c,
+                entity => $self->$method( $c, $object ),
+            );
+        }
+        else {
+            $self->status_bad_request(
+                $c,
+                message => "Unable to summarize $class objects",
+            );
+        }
+    }
+    else {
+        $self->status_not_found(
+            $c,
+            message => "Object with OID $oid not found",
+        );
+    }
+}
+
 sub investigation : Local : ActionClass('REST') {}
 
 sub investigation_GET {
@@ -48,23 +87,89 @@ sub investigation_GET {
 
     # FIXME we will undoubtedly want to expand this query further.
     my @objects = $c->model()->storage()
-                    ->select( $remote, $remote->{title}->like("%${query}%")
+                    ->select( $remote, $remote->{title}      ->like("%${query}%")
                                      | $remote->{description}->like("%${query}%") );
 
     my @data;
     foreach my $obj ( @objects ) {
-        push @data, {
-            title             => $obj->get_title(),
-            description       => $obj->get_description(),
-            date              => $obj->get_date(),
-            publicReleaseDate => $obj->get_publicReleaseDate(),
-        };
+        push @data, $self->_summarize_investigation( $c, $obj );
     }
 
     $self->status_ok(
         $c,
         entity => \@data,
     );
+}
+
+#
+# Methods used to sanitize objects before serialisation.
+#
+
+sub _summarize_investigation : Private {
+
+    my ( $self, $c, $obj ) = @_;
+
+    my %data = (
+        oid               => $c->model()->storage()->id( $obj ),
+        title             => $obj->get_title(),
+        description       => $obj->get_description(),
+        date              => $obj->get_date(),
+        publicReleaseDate => $obj->get_publicReleaseDate(),
+    );
+
+    my @factors;
+    foreach my $factor ( $obj->get_factors() ) {
+        push @factors, $self->_summarize_factor( $c, $factor );
+    }
+    $data{factors} = \@factors;
+
+    return \%data;
+}
+
+sub _summarize_factor : Private {
+
+    my ( $self, $c, $obj ) = @_;
+
+    my %data = (
+        oid               => $c->model()->storage()->id( $obj ),
+        name              => $obj->get_name(),
+        factorType        => $self->_summarize_controlled_term(
+                                         $c, $obj->get_factorType() ),
+    );
+
+    return \%data;
+}
+
+sub _summarize_controlled_term : Private {
+
+    my ( $self, $c, $obj ) = @_;
+
+    my %data = (
+        oid               => $c->model()->storage()->id( $obj ),
+        category          => $obj->get_category(),
+        value             => $obj->get_value(),
+    );
+
+    return \%data;
+}
+
+sub _summary_method : Private {
+
+    my ( $self, $class ) = @_;
+
+    # Strip the first part of the classname
+    $class =~ s/\A Bio::MAGETAB:: //xms;
+
+    # underscore separates internal capitals
+    $class =~ s/([a-z])([A-Z])/$1\_$2/g;
+
+    # substitute spaces
+    $class =~ s/\s+/\_/g;
+
+    # and then lowercase
+    $class = lc($class);
+
+    return "_summarize_$class";
 }
 
 =head1 AUTHOR
