cc8d5e14024ad67324a5fd0880bb8c61fb3e9bfc
Tim Rayner <tfrayner@gmail.com>
2009-04-07 08:02:56 +0000

    Changed variable name to something more readable.

--- lib/Bio/MAGETAB/Util/Web/Controller/Rest.pm
+++ lib/Bio/MAGETAB/Util/Web/Controller/Rest.pm
@@ -122,7 +122,7 @@ sub investigation_GET {
 
 sub _summarize_object : Private {
 
-    my ( $self, $c, $obj, $level, $class ) = @_;
+    my ( $self, $c, $obj, $depth_wanted, $class ) = @_;
 
     my $namespace = 'Bio::MAGETAB';
 
@@ -141,18 +141,18 @@ sub _summarize_object : Private {
             next CLASS unless $super =~ /^${namespace}/;
 
             # Summarize information from the superclass.
-            my $newdata = $self->_summarize_object( $c, $obj, $level, $super );
+            my $newdata = $self->_summarize_object( $c, $obj, $depth_wanted, $super );
             @{ $data }{keys %$newdata} = values %$newdata;
         }
 
         # Rather than dump everything for every object from
-        # e.g. Investigation on down, we use this $level variable to
+        # e.g. Investigation on down, we use this $depth_wanted variable to
         # track how far down in the heirarchy we want to descend. We
         # decrement after processing the superclasses.
-        $level-- if $level;
+        $depth_wanted-- if $depth_wanted;
 
         # Get information from this class.
-        my $newdata = $self->$method( $c, $obj, $level );
+        my $newdata = $self->$method( $c, $obj, $depth_wanted );
         @{ $data }{keys %$newdata} = values %$newdata;
 
         # Set some standard attributes.
@@ -172,16 +172,16 @@ sub _summarize_object : Private {
 
 # Individual class summarization methods. Within each method, only the
 # immediate attributes of each class need to be considered. Each
-# method should have two levels of verbosity: when $level evaluates
+# method should have two levels of verbosity: when $depth_wanted evaluates
 # true or not.
 
 sub _summarize_base_class : Private {
 
-    my ( $self, $c, $obj, $level ) = @_;
+    my ( $self, $c, $obj, $depth_wanted ) = @_;
 
     return unless $obj;
 
-    return {} unless $level;
+    return {} unless $depth_wanted;
 
     my %data = (
         authority => $obj->get_authority(),
@@ -193,7 +193,7 @@ sub _summarize_base_class : Private {
            
 sub _summarize_investigation : Private {
 
-    my ( $self, $c, $obj, $level ) = @_;
+    my ( $self, $c, $obj, $depth_wanted ) = @_;
 
     return unless $obj;
 
@@ -204,17 +204,17 @@ sub _summarize_investigation : Private {
         publicReleaseDate => $obj->get_publicReleaseDate(),
     );
 
-    return \%data unless $level;
+    return \%data unless $depth_wanted;
 
     my @factors;
     foreach my $factor ( $obj->get_factors() ) {
-        push @factors, $self->_summarize_object( $c, $factor, $level );
+        push @factors, $self->_summarize_object( $c, $factor, $depth_wanted );
     }
     $data{factors} = \@factors;
 
     my @sdrfs;
     foreach my $sdrf ( $obj->get_sdrfs() ) {
-        push @sdrfs, $self->_summarize_object( $c, $sdrf, $level );
+        push @sdrfs, $self->_summarize_object( $c, $sdrf, $depth_wanted );
     }
     $data{sdrfs} = \@sdrfs;
 
@@ -223,7 +223,7 @@ sub _summarize_investigation : Private {
 
 sub _summarize_sdrf : Private {
 
-    my ( $self, $c, $obj, $level ) = @_;
+    my ( $self, $c, $obj, $depth_wanted ) = @_;
 
     return unless $obj;
 
@@ -231,11 +231,11 @@ sub _summarize_sdrf : Private {
         uri               => $obj->get_uri(),
     );
 
-    return \%data unless $level;
+    return \%data unless $depth_wanted;
 
     my @sdrfrows;
     foreach my $row ( $obj->get_sdrfRows() ) {
-        push @sdrfrows, $self->_summarize_object( $c, $row, $level );
+        push @sdrfrows, $self->_summarize_object( $c, $row, $depth_wanted );
     }
     $data{sdrfRows} = \@sdrfrows;
 
@@ -244,14 +244,14 @@ sub _summarize_sdrf : Private {
 
 sub _summarize_factor : Private {
 
-    my ( $self, $c, $obj, $level ) = @_;
+    my ( $self, $c, $obj, $depth_wanted ) = @_;
 
     return unless $obj;
 
     my %data = (
         name              => $obj->get_name(),
         factorType        => $self->_summarize_object(
-                                         $c, $obj->get_factorType(), $level ),
+                                         $c, $obj->get_factorType(), $depth_wanted ),
     );
 
     return \%data;
@@ -259,21 +259,21 @@ sub _summarize_factor : Private {
 
 sub _summarize_factor_value : Private {
 
-    my ( $self, $c, $obj, $level ) = @_;
+    my ( $self, $c, $obj, $depth_wanted ) = @_;
 
     return unless $obj;
 
     my %data = (
         factor            => $self->_summarize_object(
-                                         $c, $obj->get_factor(), $level ),
+                                         $c, $obj->get_factor(), $depth_wanted ),
     );
     if ( my $term = $obj->get_term() ) {
         $data{term} = $self->_summarize_object(
-            $c, $obj->get_term(), $level );
+            $c, $obj->get_term(), $depth_wanted );
     }
     if ( my $meas = $obj->get_measurement() ) {
         $data{measurement} = $self->_summarize_object(
-            $c, $obj->get_measurement(), $level );
+            $c, $obj->get_measurement(), $depth_wanted );
     }
 
     return \%data;
@@ -281,21 +281,21 @@ sub _summarize_factor_value : Private {
 
 sub _summarize_sdrfrow : Private {
 
-    my ( $self, $c, $obj, $level ) = @_;
+    my ( $self, $c, $obj, $depth_wanted ) = @_;
 
     return unless $obj;
 
     my %data = (
         rowNumber         => $obj->get_rowNumber(),
         channel           => $self->_summarize_object(
-                                         $c, $obj->get_channel(), $level ),
+                                         $c, $obj->get_channel(), $depth_wanted ),
     );
 
-    return \%data unless $level;
+    return \%data unless $depth_wanted;
 
     my @nodedata;
     foreach my $node ( $obj->get_nodes() ) {
-        push @nodedata, $self->_summarize_object( $c, $node, $level );
+        push @nodedata, $self->_summarize_object( $c, $node, $depth_wanted );
     }
 
     $data{nodes} = \@nodedata;
@@ -305,7 +305,7 @@ sub _summarize_sdrfrow : Private {
 
 sub _summarize_node : Private {
 
-    my ( $self, $c, $obj, $level ) = @_;
+    my ( $self, $c, $obj, $depth_wanted ) = @_;
 
     return unless $obj;
 
@@ -313,7 +313,7 @@ sub _summarize_node : Private {
 
     my @commentdata;
     foreach my $comment ( $obj->get_comments() ) {
-        push @commentdata, $self->_summarize_object( $c, $comment, $level );
+        push @commentdata, $self->_summarize_object( $c, $comment, $depth_wanted );
     }
     $data{comments} = \@commentdata;
 
@@ -322,14 +322,14 @@ sub _summarize_node : Private {
     # sdrfRows for the same reason.
     my @inputedgedata;
     foreach my $input ( $obj->get_inputEdges() ) {
-        push @inputedgedata, $self->_summarize_object( $c, $input, $level );
+        push @inputedgedata, $self->_summarize_object( $c, $input, $depth_wanted );
     }
     $data{inputEdges} = \@inputedgedata;
 
     my $class = blessed $obj;
     my $method = $self->_summary_method( $class );
     if ( UNIVERSAL::can( $self, $method ) ) {
-        my %subdata = $self->$method( $c, $obj, $level );
+        my %subdata = $self->$method( $c, $obj, $depth_wanted );
         @data{ keys %subdata } = values %subdata;
     }
 
@@ -338,16 +338,16 @@ sub _summarize_node : Private {
 
 sub _summarize_data_file : Private {
 
-    my ( $self, $c, $obj, $level ) = @_;
+    my ( $self, $c, $obj, $depth_wanted ) = @_;
 
     return unless $obj;
 
     my %data = (
         uri               => $obj->get_uri(),
         format            => $self->_summarize_object(
-                                         $c, $obj->get_format(), $level ),
+                                         $c, $obj->get_format(), $depth_wanted ),
         dataType          => $self->_summarize_object(
-                                         $c, $obj->get_dataType(), $level ),
+                                         $c, $obj->get_dataType(), $depth_wanted ),
     );    
 
     return \%data;
@@ -355,7 +355,7 @@ sub _summarize_data_file : Private {
 
 sub _summarize_measurement : Private {
 
-    my ( $self, $c, $obj, $level ) = @_;
+    my ( $self, $c, $obj, $depth_wanted ) = @_;
 
     return unless $obj;
 
@@ -365,10 +365,10 @@ sub _summarize_measurement : Private {
         min_value         => $obj->get_min_value(),
         max_value         => $obj->get_max_value(),
         unit              => $self->_summarize_object(
-                                         $c, $obj->get_unit(), $level ),
+                                         $c, $obj->get_unit(), $depth_wanted ),
     );
 
-    return \%data unless $level;
+    return \%data unless $depth_wanted;
 
     # FIXME Anything here?.
     return \%data;
@@ -376,7 +376,7 @@ sub _summarize_measurement : Private {
 
 sub _summarize_database_entry : Private {
 
-    my ( $self, $c, $obj, $level ) = @_;
+    my ( $self, $c, $obj, $depth_wanted ) = @_;
 
     return unless $obj;
 
@@ -384,7 +384,7 @@ sub _summarize_database_entry : Private {
         accession         => $obj->get_accession(),
     );
 
-    return \%data unless $level;
+    return \%data unless $depth_wanted;
 
     # FIXME TermSource etc.
     return \%data;
@@ -392,7 +392,7 @@ sub _summarize_database_entry : Private {
 
 sub _summarize_controlled_term : Private {
 
-    my ( $self, $c, $obj, $level ) = @_;
+    my ( $self, $c, $obj, $depth_wanted ) = @_;
 
     return unless $obj;
 
@@ -401,7 +401,7 @@ sub _summarize_controlled_term : Private {
         value             => $obj->get_value(),
     );
 
-    return \%data unless $level;
+    return \%data unless $depth_wanted;
 
     # TermSource handled by superclass; anything else to add here?
     return \%data;
