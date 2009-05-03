9f9eaa0339323357a02acec7ac3694d5033f5c5a
Tim Rayner <tfrayner@gmail.com>
2009-05-03 11:09:08 +0000

    Merging magetab and magetab-util packages for user convenience.

--- t/testlib/CommonTests.pm
+++ t/testlib/CommonTests.pm
@@ -29,7 +29,7 @@ use Test::Exception;
 use Storable qw(dclone);
 
 use base qw(Exporter);
-our @EXPORT_OK = qw(test_class test_methods);
+our @EXPORT_OK = qw(test_class test_methods test_parse check_term);
 
 sub instantiate {
 
@@ -211,3 +211,32 @@ sub test_methods {
 
     return;
 }
+
+sub test_parse {
+
+    my ( $reader ) = @_;
+
+    $reader->parse();
+
+    return $reader->get_magetab_object();
+}
+
+sub check_term {
+
+    my ( $cat, $val, $attr, $obj, $ts, $builder ) = @_;
+
+    my $method = "get_$attr";
+
+    my $ct;
+    lives_ok( sub { $ct = $builder->get_controlled_term({
+        category   => $cat,
+        value      => $val,
+        termSource => $ts,
+    }) }, "Builder returns a $cat term" );
+    is( $ct->get_termSource(), $ts, 'with the correct termSource' );
+    is_deeply( $obj->$method(), $ct, "$attr set correctly" );
+
+    return;
+}
+
+1;
