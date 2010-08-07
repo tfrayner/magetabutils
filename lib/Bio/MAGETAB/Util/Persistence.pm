36dda45ed5cbdcbfcdfe4baa6928ff8d1f6d97ef
Tim Rayner <tfrayner@gmail.com>
2010-08-07 14:16:03 +0000

    Adding dummy classes to indicated the deprecation status of DBLoader and Persistence classes.

---
+++ lib/Bio/MAGETAB/Util/Persistence.pm
@@ -0,0 +1,67 @@
+# Copyright 2008-2010 Tim Rayner
+# 
+# This file is part of Bio::MAGETAB.
+# 
+# Bio::MAGETAB is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+# 
+# Bio::MAGETAB is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with Bio::MAGETAB.  If not, see <http://www.gnu.org/licenses/>.
+#
+# $Id$
+
+package Bio::MAGETAB::Util::Persistence;
+
+use Moose;
+
+use Carp qw(cluck);
+
+BEGIN { extends 'Bio::MAGETAB::Util::Tangram::DB'; }
+
+sub BUILD {
+
+    my ( $self, $params ) = @_;
+
+    cluck( "The Bio::MAGETAB::Util::Persistence class is deprecated and"
+         . " will shortly be removed. Please use"
+         . " Bio::MAGETAB::Util::Tangram::DB as a drop-in replacement." );
+
+    return;
+}
+
+__PACKAGE__->meta->make_immutable();
+
+no Moose;
+
+=head1 NAME
+
+Bio::MAGETAB::Util::Persistence - (DEPRECATED) Tangram-based object
+persistence class for MAGE-TAB.
+
+=head1 DESCRIPTION
+
+This class has been renamed to Bio::MAGETAB::Util::Tangram::DB.
+
+=head1 SEE ALSO
+
+L<Bio::MAGETAB::Util::Tangram::DB>
+
+=head1 AUTHOR
+
+Tim F. Rayner <tfrayner@gmail.com>
+
+=head1 LICENSE
+
+This library is released under version 2 of the GNU General Public
+License (GPL).
+
+=cut
+
+1;
