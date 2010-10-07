76bf6961ed914cf1111d453995f57723a275eec5
Tim Rayner <tfrayner@gmail.com>
2010-10-07 15:48:06 +0000

    Continuing work on the DBIC loader; almost complete _query_database and _create_object methods; early work on other required methods.

--- lib/Bio/MAGETAB/Util/DBIC/Loader.pm
+++ lib/Bio/MAGETAB/Util/DBIC/Loader.pm
@@ -54,6 +54,52 @@ sub _manage_namespace_authority {
     }
 }
 
+sub _camel_case_tab_delim {
+
+    # Convert a CamelCase class name (without the Bio::MAGETAB::
+    # prefix) to a tab-delimited form.
+    my ( $self, $str ) = @_;
+
+    $str =~ s/([a-z])([A-Z])/${1}_${2}/g;
+
+    return(lc($str));
+}
+
+sub _magetab_to_relname {
+
+    # Given a MAGETAB object, derive the name of the relationship in
+    # which it might be found.
+    my ( $self, $obj ) = @_;
+
+    my $objclass = ref $obj;
+    $objclass =~ s/.*:://;
+    my $method = $self->_camel_case_tab_delim( $objclass );
+
+    return( $method );
+}
+
+sub _magetab_to_attrs_and_idfields {
+
+    # Given a MAGETAB object, retrieve the ID fields defined by the
+    # Builder superclass and create an attr hashref suitable for use
+    # with _query_database.
+    my ( $self, $obj ) = @_;
+
+    # FIXME can this be done more robustly?
+    my $method = $self->_magetab_to_relname( $obj );
+    my ( $class, @id_fields ) = @self->_retrieve_id_fields( $method );
+
+    # Quick assertion to make sure we're on the right track.
+    if ( $class ne ref $obj ) {
+        confess("Internal Error: unexpected class returned by _retrieve_id_fields.");
+    }
+
+    my %attrhash = map { my $getter = "get_$_";
+                         $_ => $value->$getter } @id_fields;
+    
+    return ( \%attrhash, \@id_fields );
+}
+
 sub _query_database {
 
     my ( $self, $class, $data, $id_fields ) = @_;
@@ -64,10 +110,6 @@ sub _query_database {
               . qq{ Must use at least one of the following: $allowed.\n});
     }
 
-    # FIXME do we need $clean_data at all?
-    my ( $clean_data, $aggregators )
-        = $self->_strip_aggregator_info( $class, $data );
-
     # Add authority, namespace to $id_fields unless $data has a
     # termSource.  Also, TermSources themselves are *always* treated
     # as global in this way.
@@ -75,22 +117,20 @@ sub _query_database {
     $id_fields = [ keys %tmp_fields ];
     $self->_manage_namespace_authority( $data, $class );
 
-    my $query;
+    my ( $cond, $attr );
     FIELD:
     foreach my $field ( @{ $id_fields } ) {
 
         my $value = $data->{ $field };
 
-        # Don't add aggregator fields to the query (the schema doesn't
-        # know about them). FIXME this will change, I hope.
-        next FIELD if ( first { $field eq $_ } @{ $aggregators } );
+        next FIELD if ! defined $value;
 
         # Another special case - URI can change in the model
         # between input and output (specifically, a file: prefix
         # may be added). This is copied from
-        # Bio::MAGETAB::Types. FIXME date will need the same
-        # treatment.
-        if ( defined $value && $field eq 'uri' ) {
+        # Bio::MAGETAB::Types. N.B. dates will need the same
+        # treatment if they are used as ID fields.
+        if ( $field eq 'uri' ) {
             use URI;
             $value = URI->new( $value );
 
@@ -100,19 +140,46 @@ sub _query_database {
             }
         }
 
-        # FIXME this is sadly simplistic - we need to serialise
-        # Bio::MAGETAB objects such that the database knows what to do
-        # about them. Note also that this could get seriously recursive.
-        $query->{ $field } = $value;
+        # We need to serialise Bio::MAGETAB objects such that the
+        # database knows what to do about them. Note also that this
+        # could get quite recursive. (FIXME include a check for cycles
+        # here).
+        if ( $value->isa('Bio::MAGETAB::BaseClass') ) {
+
+            # Note that we're not carrying over namespace, authority
+            # because some objects (TermSource) might not have them.
+            my ( $rel_attrs, $rel_id_fields ) = $self->_magetab_to_attrs_and_idfields( $value );
+            my $relobj = $self->_query_database( ref $value, $rel_attrs, $rel_id_fields );
+
+            # Special cases.
+            if ( ( $class eq 'Bio::MAGETAB::Feature'     && $field eq 'array_design' )
+              || ( $class eq 'Bio::MAGETAB::Measurement' && $field eq 'object' ) ) {
+
+                # The database and Bio::MAGETAB model all allow n..n
+                # for Measurement..Object, but the Builder classes
+                # don't work that way. We need a join, as for Feature.
+                my $relname = $self->_magetab_to_relname( $value );
+                $cond->{ "${relname}s.id" } = $relobj->id();
+                $attr->{ 'join' } = { "${relname}s" };
+            }
+            else {
+
+                # The general case.
+                $cond->{ $field } = $relobj;
+            }
+        }
+        elsif ( ref $value eq 'ARRAY' ) {
+            # No-op; currently this is not needed. There are no
+            # Builder ID fields which point to one-to-many or
+            # many-to-many relationships.
+        }
+        else {
+            $cond->{ $field } = $value;
+        }
     }
 
-    # FIXME I think we can add aggregate methods to the ORM and then
-    # directly query on such objects here. Note that some classes
-    # e.g. Feature will require some special handling (IN
-    # array_designs, rather than =array_design, that kind of
-    # thing.). Also Measurement needs a search across materials,
-    # factor_value and parameter_value.
-    my @objects = $self->get_database->resultset( $class )->search( $query );
+    # Actually retrieve the object(s).
+    my @objects = $self->get_database->resultset( $class )->search( $cond, $attr );
 
     # Brief sanity check; identity means identity, i.e. only one object returned.
     if ( scalar @objects > 1 ) {
@@ -126,10 +193,12 @@ sub _query_database {
 
 sub _get_object {
 
+    # Takes a class name, attr hashref and ID fields arrayref. Returns
+    # a Bio::MAGETAB object.
     my ( $self, $class, $data, $id_fields ) = @_;
 
-    if ( my $row = $self->_query_database( $class, $data, $id_fields ) ) {
-        return $self->_dbrow_to_magetab( $row );
+    if ( my $dbrow = $self->_query_database( $class, $data, $id_fields ) ) {
+        return $self->_dbrow_to_magetab( $dbrow );
     }
     elsif ( $self->get_relaxed_parser() ) {
 
@@ -152,29 +221,82 @@ sub _get_object {
 
 sub _create_object {
 
+    # Takes a class name, attr hashref and ID fields arrayref. Returns
+    # a Bio::MAGETAB object representing a newly-created DB row.
     my ( $self, $class, $data, $id_fields ) = @_;
 
-    # Strip out aggregator identifier components
-    $data = $self->_strip_aggregator_info( $class, $data );
+    # Get a listing of aggregator fields as $is_aggregator
+    my ( $cleaned, $is_aggregator ) = $self->_strip_aggregator_info( $class, $data );
 
     # Make sure our authority and namespace attributes are
     # appropriately managed.
     $self->_manage_namespace_authority( $data, $class );
 
-    # FIXME we need to create a DBIC row from the data; the following
-    # UNTESTED should in theory work. FIXME a better error message
-    # would dump the data hash as well.
-    my $row = $self->database->resultset( $class )->create( $data )
+    # We need to create a DBIC row from the data. This also needs to
+    # handle retrieval (but *not* creation) of linked objects. We rely
+    # on the consumer of the Builder object to get the object creation
+    # order right.
+    my %query;
+    foreach my $field ( keys %$data ) {
+
+        my $value = $data->{ $field };
+        if ( $value->isa( 'Bio::MAGETAB::BaseClass' ) ) {
+
+            # Replace this hash value with the appropriate database
+            # ID. Note that $field may need to be altered as well,
+            # especially for Feature and Measurement.
+            my ( $rel_attrs, $rel_id_fields ) = $self->_magetab_to_attrs_and_idfields( $value );
+            my $relobj = $self->_query_database( ref $value, $rel_attrs, $rel_id_fields );
+
+            if ( ( $class eq 'Bio::MAGETAB::Feature'     && $field eq 'array_design' )
+              || ( $class eq 'Bio::MAGETAB::Measurement' && $field eq 'object' ) ) {
+                my $relname = $self->_magetab_to_relname( $value );
+                $query{ $relname } = $relobj;
+            }
+            else {
+                $query{ $field } = $relobj;
+            }
+        }
+        elsif ( ref $value eq 'ARRAY' ) {
+            
+            # We can safely assume that $value is an arrayref of
+            # Bio::MAGETAB objects rather than anything else (this was
+            # a core design decision when creating the model). We also
+            # assume that these are not aggregator identifier
+            # components, since there are no multi-style aggregator
+            # fields specified by the Builder class.
+            my @relobjs;
+            foreach my $val ( @$value ) {
+                my ( $rel_attrs, $rel_id_fields ) = $self->_magetab_to_attrs_and_idfields( $value );
+                my $relobj = $self->_query_database( ref $value, $rel_attrs, $rel_id_fields );
+
+                push @relobjs, $relobj;
+            }
+
+            # According to the DBIx::Class::ResultSet docs this should
+            # Just Work.
+            $query{ $field } = \@relobjs;
+        }
+        else {
+
+            # The more general case.
+            $query{ $field } = $value;
+        }
+    }
+
+    # Actually insert the row. FIXME a better error message would dump
+    # the data hash as well.
+    my $dbrow = $self->database->resultset( $class )->create( \%query )
         or confess("Error creating object of class $class");
 
-    # We need to convert the DBIC row object $row into a Bio::MAGETAB
+    # We need to convert the DBIC row object $dbrow into a Bio::MAGETAB
     # object.
-    return $self->_dbrow_to_magetab( $row );
+    return $self->_dbrow_to_magetab( $dbrow );
 }
 
 sub _dbrow_to_magetab { # TODO
 
-    my ( $self, $row ) = @_;
+    my ( $self, $dbrow ) = @_;
 
     # FIXME Convert the DBIC row into a Bio::MAGETAB object. This
     # wants to be a bit clever about how it handles Bio::MAGETAB
@@ -182,6 +304,38 @@ sub _dbrow_to_magetab { # TODO
     # a closure method which knows about the attribute data and, when
     # called, replaces itself with an instantiated MAGETAB object.
     my $obj;
+
+    my $class = $dbrow->result_source->source_name();
+    $class =~ s/ .*:: //xms;
+    $class = 'Bio::MAGETAB::$class';
+
+    my $source = $dbrow->result_source();
+    foreach my $attr ( $class->meta->get_attribute_list() ) {
+        if ( $source->has_relationship( $attr ) ) {
+
+            # Instead of instantiating the related Bio::MAGETAB
+            # object(s), we create a closure which can replace itself
+            # when called. This helps avoid dumping out the entire
+            # database for a simple query.
+            $obj->{ $attr } =
+                sub {
+
+                    # FIXME this only works for the has_one case.
+                    $obj->{ $attr } = $self->_dbrow_to_magetab( $dbrow->$attr );
+                };
+        }
+        elsif ( $source->has_column( $attr ) && defined $dbrow->$attr ) {
+
+            # Simple attribute. N.B. we may need to handle
+            # interconversion between MySQL and Bio::MAGETAB
+            # conventions, e.g. for date.
+            $obj->{ $attr } = $dbrow->$attr;
+        }
+    }
+
+    # This circumvents the usual type constraint checking, allowing us
+    # to use closures instead of instantiated Bio::MAGETAB objects.
+    bless $obj, $class;
     
     return $obj;
 }
@@ -201,32 +355,46 @@ sub update { # TODO
 
 sub _update_dbrow_attributes { # TODO
 
-    my ( $self, $row, $data ) = @_;
+    my ( $self, $dbrow, $data ) = @_;
 
-    # FIXME just apply the $data hashref to the $row DBIC object, and call $row->update;
+    # FIXME just apply the $data hashref to the $dbrow DBIC object, and call $dbrow->update;
 
     # Note this won't be quite as simple if the $data contains Bio::MAGETAB objects as values.
+    my $source = $dbrow->result_source();
+    while ( my ( $attr, $value ) = each %$data ) {
+        if ( $source->has_column( $attr ) ) {
+            $dbrow->set_column( $attr, $value );
+        }
+        elsif ( $source->has_relationship( $attr ) ) {
+            # FIXME
+        }
+        else {
 
-    $row->update();
+            # FIXME is this actually a full-blown error?
+            carp(sprintf("Warning: %s class has no %s attribute",
+                         $source->source_name(), $attr));
+        }
+    }
+
+    $dbrow->update();
 }
 
 sub _find_or_create_object {
 
     my ( $self, $class, $data, $id_fields ) = @_;
 
-    my $row = $self->_query_database( $class, $data, $id_fields );
+    my $dbrow = $self->_query_database( $class, $data, $id_fields );
 
     # Strip out aggregator identifier components
     $data = $self->_strip_aggregator_info( $class, $data );
 
     my $obj;
-    if ( $row ) {
+    if ( $dbrow ) {
 
-        # Update the old object as appropriate. FIXME this needs
-        # writing to take a DBIC::Row object.
-        $self->_update_dbrow_attributes( $row, $data );
+        # Update the old dbrow as appropriate.
+        $self->_update_dbrow_attributes( $dbrow, $data );
 
-        $obj = $self->_dbrow_to_magetab( $obj );
+        $obj = $self->_dbrow_to_magetab( $dbrow );
     }
     else {
 
